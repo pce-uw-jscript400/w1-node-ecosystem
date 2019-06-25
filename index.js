d61bfe9087479d9ae22135c9b808a9a67f57e30a
Colin Griffiths <colinrossgriffiths@gmail.com>
2019-06-25 13:42:05 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const profile = require('./src/profile.js')
+const path = require('path')
+const moment = require('moment')
+console.log(path.resolve())
+console.log('Hello, Node!')
+console.log(profile.name)
+console.log(profile.birthday)
---
+++ src/profile.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'Colin Griffiths',
+  birthday: '5/20/1982'
+}
