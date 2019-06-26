1fe0e6619eb2277a1f2ff487dd5a66d53ea404a3
Shahab13 <sam.autom@gmail.com>
2019-06-25 19:36:13 -0700

    moment.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, Node!");
+// const profile = require("./profile.js");
+// console.log(profile);
+
+// console.log("Hello, Node!");
+
+// const path = require("path");
+// console.log(path.resolve());
+const moment = require("moment");
+
+console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
